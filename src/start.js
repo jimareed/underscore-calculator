29d135fc21801cf2915739fdbb7846dfa578abbd
jimareed <jim.a.reed@gmail.com>
2015-06-23 13:07:17 -0400

    added stub store/recall

--- src/start.js
+++ src/start.js
@@ -3,6 +3,11 @@ var app = express();
 var exphbs = require('express-handlebars');
 var calculator = require('./calculator');
 
+var response = [{ country:"China" , population:1343 } ,
+              { country:"India" , population:1205 } ,
+              { country:"United States" , population:314 } ,
+              { country:"Brazil" , population:194 }];
+
 app.engine('handlebars',
 	exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -20,6 +25,20 @@ app.get('/underscore-calculator', function(req, res) {
 app.get('/underscore-calculator/calculate/:op', function(req, res) {
 	var theRegisters = calculator.evaluate(req.params.op,req.query.input);
 	var theData = JSON.stringify(theRegisters);
+	res.render('display', { data : theData });
+});
+
+app.get('/underscore-calculator/recall/:name', function(req, res) {
+	console.log("recall " + req.params.name);
+	var input = JSON.stringify(response);
+	var theRegisters = calculator.evaluate("enter",input);
+	var theData = JSON.stringify(theRegisters);
+	res.render('display', { data : theData });
+});
+
+app.get('/underscore-calculator/store/:name', function(req, res) {
+	console.log("store " + req.params.name);
+	var theData = JSON.stringify(calculator.evaluate("", ""));
 	res.render('display', { data : theData });
 });
 
