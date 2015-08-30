5aae3010dd65e92eea55e6e5dea5bd262ebeafdb
Jim Reed <ire@us-rem-ire.local>
2015-08-30 10:51:18 -0400

    added file upload view

--- src/start.js
+++ src/start.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 var exphbs = require('express-handlebars');
 var calculator = require('./calculator');
+var busboy = require('connect-busboy'); //middleware for form/file upload
 
 app.engine('handlebars',
 	exphbs({defaultLayout: 'main'}));
@@ -17,6 +18,10 @@ app.get('/underscore-calculator', function(req, res) {
 	res.render('display', { data : theData });
 });
 
+app.get('/underscore-calculator/upload-file', function(req, res) {
+	res.render('upload-file');
+});
+
 app.get('/underscore-calculator/:func/:op', function(req, res) {
 	var theData = JSON.stringify(calculator.evaluate(req.params.func, req.params.op,req.query.input));
 	res.render('display', { data : theData });
@@ -24,6 +29,22 @@ app.get('/underscore-calculator/:func/:op', function(req, res) {
 
 app.use(express.static('public'));
 
+//
+// file upload route
+//
+app.use(busboy());
+app.route('/upload')
+    .post(function (req, res, next) {
+
+        req.pipe(req.busboy);
+        req.busboy.on('file', function (fieldname, file, filename) {
+            console.log("Uploading: " + filename);
+        });
+    });
+
+//
+// listener
+//
 var port = Number(process.env.PORT || 8080);
 app.listen(port);
 
